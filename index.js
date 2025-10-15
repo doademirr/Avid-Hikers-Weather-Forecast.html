e39b8c98487a5d6dc21eba4c41b041240ead1663
Doa <dogademirr@gmail.com>
2025-10-15 15:54:17 +1100

    Added axios, API key and used JavaScript to update temperature

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-function searchCity(event) {
+function submitCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-input");
   let currentCity = document.querySelector(".current-city");
   currentCity.innerHTML = searchInput.value;
+  searchCityElement(searchInput.value);
 }
 
 let searchCityElement = document.querySelector(".search-box");
-searchCityElement.addEventListener("submit", searchCity);
+searchCityElement.addEventListener("submit", submitCity);
+
+function searchCity(city) {
+  let key = bd40oa3ta2b94eabedb83bb3022b94f7;
+  let apiKey = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+  axios.get(apiKey).then(updateTemperature);
+}
+
+function updateTemperature(response) {
+  let currentTemperatureValue = document.querySelector(
+    ".current-temperature-value"
+  );
+  let currentTemperatureValueRounded = response.data.temperature.current;
+  currentTemperatureValue.innerHTML = Math.round(
+    currentTemperatureValueRounded
+  );
+}
