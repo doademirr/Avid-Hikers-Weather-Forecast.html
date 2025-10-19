bf611fbc1f9081440e6c43a8256cc130bd13ef85
Doa <dogademirr@gmail.com>
2025-10-20 10:58:01 +1100

    Current date updates dynamically
    Completed the JavaScript work to have the current date update dynamically. Formatted the hours, days and minutes.

--- index.js
+++ index.js
@@ -33,8 +33,28 @@ function updateTemperature(response) {
   humidity.innerHTML = `${response.data.temperature.humidity}%`;
   let wind = document.querySelector("#wind");
   wind.innerHTML = `${response.data.wind.speed}km/h`;
-  let currentDate = new Date(response.data.time * 1000);
-  currentDate.innerHTML = `${Date.getDay()} ${Date.getHours()}:${Date.getMinutes()}`;
+  let currentDate = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
+  currentDate.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 searchCity("Hobart");
